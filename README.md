a6d895e6e0e733846535b8caa16bc2477862171c
Deepak Bhattarai <bring2dip@gmail.com>
2021-01-12 17:59:31 +0545

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# react-portal-base
-React Portal for rendering modal and notifications
+# React Portal Base
+Simple React Portal for rendering modal and notifications
+
+## Install
+
+````
+npm i react-portal-base
+or
+yarn add react-portal-base
+````
+
+##Setup
+Add root element in your view.
+````
+<!--For notification-->
+<div 
+  id="react-portal-base-notification-root"
+  class="react-portal-base-notification-root"
+>
+</div>
+<!--For modal-->
+<div 
+  id="react-portal-base-modal-root"
+  class="react-portal-base-modal-root"
+>
+</div>
+````
+
+
+##Modal
+
+````
+import React from 'react';
+import Portal from 'react-portal-base';
+import ModalComponent from 'some modal';
+
+export default ModalInPortalTest() {
+  
+  return (
+    <Portal portalType="modal">
+     <ModalComponent />
+    </Portal>
+  );
+}
+````
+
+##Notifiction
+Children inside notification portal disappear after `timeout` in ms. Default is `3000ms`.
+````
+import React from 'react';
+import Portal from 'react-portal-base';
+import Alert from 'some alert component';
+
+export default ModalInPortalTest() {
+  
+  return (
+    <Portal portalType="notification" timeout={3000}>
+     <Alert />
+    </Portal>
+  );
+}
+````
+
+## API
+
+````
+timeout: Timeout in Number (Default 3000 ms),
+portalType: One of ['modal', 'notification'] (Required)
+notificationRootId: custom notification root id (Default: react-portal-base-notification-root),
+modalRootId: custom notification root id (Default: react-portal-base-modal-root),
+````
+
+## License
+
+MIT
---
+++ src/Portal/Portal.js
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default function Portal({ portalType, notificationRootId, modalRootId, timeout, children }) {
+  const [element, setElement] = useState(null);
+  const portalRoot = useRef(null);
+
+  const idMap = {
+    notification: notificationRootId,
+    modal: modalRootId,
+  };
+
+  const removeChild = () => {
+    if (element && element.parentNode === portalRoot.current) {
+      portalRoot.current.removeChild(element);
+    }
+  };
+
+  useEffect(function setRootElementBasedOnPortalType() {
+    portalRoot.current = document.getElementById(idMap[portalType]);
+    if (portalType === 'notification') {
+      setElement(document.createElement('div'));
+    } else {
+      setElement(portalRoot.current);
+    }
+  }, []);
+
+  useEffect(function appendElementForNotification() {
+    if (element && portalType === 'notification') {
+      portalRoot.current.appendChild(element);
+    }
+  }, [element]);
+
+  useEffect(function handleNotificationTimeout() {
+    let timeoutId = null;
+    if (element && portalType === 'notification') {
+      timeoutId = setTimeout(() => {
+        removeChild();
+      }, timeout);
+    }
+    return function cleanup() {
+      clearInterval(timeoutId);
+      removeChild();
+    };
+  }, [element]);
+
+  if (!element) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(children, element);
+}
+
+Portal.propTypes = {
+  children: PropTypes.node.isRequired,
+  timeout: PropTypes.number,
+  portalType: PropTypes.string.oneOf(['modal', 'notification']).isRequired,
+  notificationRootId: PropTypes.string,
+  modalRootId: PropTypes.string,
+};
+
+Portal.defaultProps = {
+  timeout: 3000,
+  notificationRootId: 'react-portal-base-notification-root',
+  modalRootId: 'react-portal-base-modal-root',
+};
